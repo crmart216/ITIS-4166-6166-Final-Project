10553ea0338084544e2d87f79f83ab1d39b4bca5
Ryan Martin <cmart216@uncc.edu>
2025-10-28 12:07:14 -0400

    Created repo and added starter code

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV === 'development') {
+    const morganModule = await import('morgan');
+    const morgan = morganModule.default;
+    app.use(morgan('tiny'));
+}
+app.use(express.json());
+
+/* Inlcude routes here */
+
+
+
+/* Basic error handling*/
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if(!err.status) {
+        err.status = 500;
+        err.message = 'Internal Server Error';
+    }
+    res.status(err.status).json({error: err.message});
+});
+/* Start server */
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
