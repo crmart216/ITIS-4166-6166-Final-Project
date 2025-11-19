0ea5bb3647164d8009281d45815dd7193161119d
cmart216 <cmart216@uncc.edu>
2025-11-19 12:08:57 -0500

    fixed jwt token expire error

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -15,6 +15,8 @@ export const protectRoute = async (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized - Invalid Token" });
     }
 
+
+
     const user = await findUsersByEmail(decoded.email);
     if (!user) {
       return res.status(401).json({ message: "Unauthorized - Invalid Token" });
@@ -25,7 +27,11 @@ export const protectRoute = async (req, res, next) => {
     req.user = userNoPassword;
     next();
   } catch (error) {
-    console.log("Error in protectRoute middleware:", error);
-    res.status(500).json({ message: "Internal server error" });
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Unauthroized - Expired Token, Please log in again"});
+    } else {
+      console.log("Error in protectRoute middleware:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
   }
 };
